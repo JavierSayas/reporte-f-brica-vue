f862bbae62986c755f3157e3658dcb741581325c
JavierSayas <javiersayas@surexport.es>
2025-07-31 12:24:19 +0200

    Create FormEmpaquetado.vue

---
+++ src/components/forms/FormEmpaquetado.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { toRefs } from 'vue';
+
+const props = defineProps({
+  modelValue: Object,
+});
+defineEmits(['update:modelValue']);
+
+const { coordinador, incidencias } = toRefs(props.modelValue);
+</script>
+
+<template>
+  <div>
+    <label class="block text-gray-700 text-sm font-bold mb-2">COORDINADOR:</label>
+    <input type="text" v-model="coordinador" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700" />
+    <label class="block text-gray-700 text-sm font-bold mb-2 mt-4">Incidencias:</label>
+    <textarea v-model="incidencias" rows="3" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"></textarea>
+  </div>
+</template>
