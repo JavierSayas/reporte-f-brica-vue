8971b8d17e12a1d04fee874337f54f7782e14e1d
JavierSayas <javiersayas@surexport.es>
2025-08-04 15:33:10 +0200

    Update FormEmpaquetado.vue

--- src/components/forms/FormEmpaquetado.vue
+++ src/components/forms/FormEmpaquetado.vue
@@ -1,19 +1,22 @@
 <script setup>
-import { toRefs } from 'vue';
+import { computed } from 'vue';
 
 const props = defineProps({
   modelValue: Object,
 });
-defineEmits(['update:modelValue']);
+const emit = defineEmits(['update:modelValue']);
 
-const { coordinador, incidencias } = toRefs(props.modelValue);
+const formData = computed({
+  get: () => props.modelValue,
+  set: (value) => emit('update:modelValue', value),
+});
 </script>
 
 <template>
-  <div>
+  <div v-if="formData">
     <label class="block text-gray-700 text-sm font-bold mb-2">COORDINADOR:</label>
-    <input type="text" v-model="coordinador" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700" />
+    <input type="text" v-model="formData.coordinador" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700" />
     <label class="block text-gray-700 text-sm font-bold mb-2 mt-4">Incidencias:</label>
-    <textarea v-model="incidencias" rows="3" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"></textarea>
+    <textarea v-model="formData.incidencias" rows="3" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"></textarea>
   </div>
 </template>
